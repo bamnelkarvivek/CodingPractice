cfa83a0a8fe9304e81f6f79d25dea3f1e579516d
bamnelkarvivek <bamnelkarvivek@gmail.com>
2025-09-19 17:33:16 -0400

    Added reversed() method to Singly_linked_list to return the elements in reversed order

--- Singly_linked_list.py
+++ Singly_linked_list.py
@@ -1,28 +1,39 @@
 class Node:
+    """A node in a singly linked list"""
     def __init__(self,d,n = None):
         self.data = d
         self.next_node = n
+
     def get_data(self):
         return self.data
+    
     def set_data(self,d):
         self.data = d
+
     def get_next(self):
         return self.next_node
+    
     def set_next(self,n):
         self.next_node = n
         
 class LinkedList:
+    """A simple singly linked list implementation."""
     def __init__(self,r = None):
         self.root = r
         self.size = 0
+
     def add(self,d):
+        """Add a new node containing data to the head of the list."""
         new_node = Node(d,self.root)
         self.root = new_node
         self.size += 1
+
     def remove(self,d):
+        """Remove the first node containing the specified data."""
         prev_node = None
         this_node = self.root
         next_node = None
+
         while this_node:
             next_node = this_node.get_next()
             if this_node.get_data() == d:
@@ -36,6 +47,7 @@ class LinkedList:
                 prev_node = this_node
                 this_node = next_node
         return False
+    
     def __str__(self):
         """Return a string representation of the list."""
         nodes = []
@@ -44,6 +56,15 @@ class LinkedList:
             nodes.append(str(current.get_data()))
             current = current.get_next()
         return " -> ".join(nodes) if nodes else "Empty List"
+    
+    def __reversed__(self):
+        nodes = []
+        current = self.root
+        # iterate over the list
+        while current:
+            nodes.append(str(current.get_data()))
+            current = current.get_next()
+        return " -> ".join(reversed(nodes)) if nodes else "Empty list."
 
 if __name__ == "__main__":
     #Example Usage
@@ -54,6 +75,7 @@ if __name__ == "__main__":
 
     print("Initial list:",ll) # Output: 15 -> 10 -> 5
     print("Size:",ll.size)    # Output: 3
+    print("Reversed list:", reversed(ll)) # Output: 5 -> 10 -> 15
 
     ll.remove(10)
     print("After removing 10:", ll) #Output: 15 -> 5
